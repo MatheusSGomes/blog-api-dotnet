b2afc701469197b51b4de8187e49b4f1f23276ed
Matheus Gomes <math.s.gomes@gmail.com>
2024-08-09 23:09:41 -0300

    add category relationship with article

--- Blog/Domain/Article.cs
+++ Blog/Domain/Article.cs
@@ -5,4 +5,6 @@ public class Article
     public Guid Id { get; set; }
     public string Title { get; set; }
     public string Content { get; set; }
+    public Guid CategoryId { get; set; }
+    public Category Category { get; set; }
 }
---
+++ Blog/Migrations/20240810020816_AddCategoryAsRelationOfArticle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryAsRelationOfArticle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CategoryId",
+                table: "Articles",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_CategoryId",
+                table: "Articles",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Articles_Categories_CategoryId",
+                table: "Articles",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Articles_Categories_CategoryId",
+                table: "Articles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Articles_CategoryId",
+                table: "Articles");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Articles");
+        }
+    }
+}
