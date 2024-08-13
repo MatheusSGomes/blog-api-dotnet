c0bcd1641a4e16f9cc799eef3e37db56f2b5e284
Matheus Gomes <math.s.gomes@gmail.com>
2024-08-12 23:59:16 -0300

    refact: add async to category post

--- Blog/UseCases/Categories/CategoryPost.cs
+++ Blog/UseCases/Categories/CategoryPost.cs
@@ -11,15 +11,15 @@ public class CategoryPost
     public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
     public static Delegate Handle => Action;
 
-    public static IResult Action([FromBody] CategoryRequest categoryRequest, ApplicationDbContext context)
+    public static async Task<IResult> Action([FromBody] CategoryRequest categoryRequest, ApplicationDbContext context)
     {
         var category = new Category(name: categoryRequest.Name);
 
         if (!category.IsValid)
             return Results.BadRequest(category.Notifications);
 
-        context.Categories.Add(category);
-        context.SaveChanges();
+        await context.Categories.AddAsync(category);
+        await context.SaveChangesAsync();
         return Results.Created("/category", category.Id);
     }
 }
