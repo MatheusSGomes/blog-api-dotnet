86fd2b5c58acf4cc4a59dea7a76df1bf31400a3d
Matheus Gomes <math.s.gomes@gmail.com>
2024-08-20 22:18:56 -0300

    create token jwt

--- Blog/UseCases/Auth/TokenPost.cs
+++ Blog/UseCases/Auth/TokenPost.cs
@@ -1,6 +1,9 @@
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Text;
 using Blog.Exception;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Blog.UseCases.Auth;
 
@@ -10,7 +13,7 @@ public class TokenPost
     public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
     public static Delegate Handle => Action;
 
-    public static async Task<IResult> Action(LoginRequest request, UserManager<IdentityUser> userManager)
+    public static async Task<IResult> Action(LoginRequest request, UserManager<IdentityUser> userManager, IConfiguration configuration)
     {
         var user = await userManager.FindByEmailAsync(request.Email);
 
@@ -32,6 +35,22 @@ public class TokenPost
 
         var subject = new ClaimsIdentity(aditionalClaims);
         subject.AddClaims(userClaims);
+
+        var key = Encoding.ASCII.GetBytes(configuration["JwtBearerTokenSettings:SecretKey"]);
+
+        var symetricSecurityKey = new SymmetricSecurityKey(key);
+        
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = subject,
+            Audience = "Blog",
+            Issuer = "Issuer",
+            Expires = DateTime.UtcNow.AddDays(2),
+            SigningCredentials = new SigningCredentials(symetricSecurityKey, SecurityAlgorithms.HmacSha256Signature),
+        };
+
+        var tokenHanlder = new JwtSecurityTokenHandler();
+        var token = tokenHanlder.CreateToken(tokenDescriptor);
 
         return Results.Ok(user);
     }
