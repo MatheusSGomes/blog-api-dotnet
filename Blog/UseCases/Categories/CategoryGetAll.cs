5601f778457fd6c2f8c0a25ce246342f69d8daa9
Matheus Gomes <math.s.gomes@gmail.com>
2024-08-23 23:59:24 -0300

    add pagination category get all

--- Blog/UseCases/Categories/CategoryGetAll.cs
+++ Blog/UseCases/Categories/CategoryGetAll.cs
@@ -1,5 +1,6 @@
 using Blog.Infrastructure;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace Blog.UseCases.Categories;
@@ -11,9 +12,13 @@ public class CategoryGetAll
     public static Delegate Handle => Action;
 
     [AllowAnonymous]
-    public static async Task<IResult> Action(ApplicationDbContext context)
+    public static async Task<IResult> Action(ApplicationDbContext context, [FromQuery] int page = 1, [FromQuery] int rows = 5)
     {
-        var categories = await context.Categories.ToListAsync();
+        var categories = await context.Categories
+            .Skip((page - 1) * rows)
+            .Take(rows)
+            .ToListAsync();
+
         var response = categories.Select(c => new CategoryResponse(Id: c.Id, Name: c.Name));
         return Results.Ok(response);
     }
