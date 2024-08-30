0e0a77ca3c31c030e0769a2dcd300891aca7a553
Matheus Gomes <math.s.gomes@gmail.com>
2024-08-29 23:04:53 -0300

    add rabbitmq consumer to increment article counter views

--- Blog/UseCases/CounterViews/ConsumerArticleCounterViews.cs
+++ Blog/UseCases/CounterViews/ConsumerArticleCounterViews.cs
@@ -1,5 +1,6 @@
 using System.Text;
 using Blog.Infrastructure;
+using Blog.UseCases.CounterViews;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using RabbitMQ.Client;
@@ -16,41 +17,7 @@ public class ConsumerArticleCounterViews
     [AllowAnonymous]
     public static void ReceiveMessage(ApplicationDbContext context)
     {
-        var factory = new ConnectionFactory { HostName = "localhost" };
-        using var connection = factory.CreateConnection();
-        using var channel1 = connection.CreateModel();
-
-        channel1.QueueDeclare(
-            queue: "articleCounterViews",
-            durable: false,
-            exclusive: false,
-            autoDelete: false,
-            arguments: null);
-        
-        Console.WriteLine(" [*] Waiting for messages.");
-
-        var consumer = new EventingBasicConsumer(channel1);
-
-        consumer.Received += async void (objectModel, deliverEventArgs) =>
-        {
-            // TODO: Ajuste problema "Cannot access a disposed context instance."
-            var body = deliverEventArgs.Body.ToArray();
-            var articleId = Encoding.UTF8.GetString(body);
-
-            var article = await context.Articles.FindAsync(articleId);
-
-            if (article != null)
-            {
-                article.CountViews += 1;
-                context.SaveChanges();
-            }
-
-            Console.WriteLine($" [x] Recebido: {articleId}");
-        };
-
-        channel1.BasicConsume(
-            queue: "articleCounterViews",
-            autoAck: true,
-            consumer: consumer);
+        var consumer = new RabbitMQConsumer(context);
+        consumer.Start();
     }
 }
---
+++ Blog/UseCases/CounterViews/RabbitMQConsumer.cs
@@ -0,0 +1,61 @@
+using System.Text;
+using Blog.Infrastructure;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Blog.UseCases.CounterViews;
+
+public class RabbitMQConsumer
+{
+    private readonly ApplicationDbContext _context;
+
+    public RabbitMQConsumer(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public void Start()
+    {
+        var factory = new ConnectionFactory { HostName = "localhost" };
+        using var connection = factory.CreateConnection();
+        using var channel1 = connection.CreateModel();
+
+        channel1.QueueDeclare(
+            queue: "articlesQueue",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null);
+
+        var consumer = new EventingBasicConsumer(channel1);
+
+        consumer.Received += async void (objectModel, deliverEventArgs) =>
+        {
+            var body = deliverEventArgs.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+
+            await IncrementArticleViewAsync(message);
+        };
+
+        channel1.BasicConsume(
+            queue: "articleCounterViews",
+            autoAck: true,
+            consumer: consumer);
+        
+        Console.WriteLine("Press [enter] to exit.");
+        Console.ReadLine();
+    }
+
+    private async Task IncrementArticleViewAsync(string articleId)
+    {
+        var parseId = Guid.Parse(articleId);
+        var article = await _context.Articles.FindAsync(parseId);
+
+        if (article != null)
+        {
+            article.CountViews++;
+            _context.Articles.Update(article);
+            _context.SaveChanges();
+        }
+    }
+}
